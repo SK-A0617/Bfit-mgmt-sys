c8c13b8eef1e82c77e90aa516fb0a76dc67d8d51
SK-A0617 <aakashmoorthy16@gmail.com>
2024-12-13 16:56:37 +0530

    S3 config added with region bug

--- src/main/java/com/bfit/mgmt/config/S3Config.java
+++ src/main/java/com/bfit/mgmt/config/S3Config.java
@@ -1,19 +1,40 @@
 package com.bfit.mgmt.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import jakarta.annotation.PostConstruct;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
 
+	@Value("${cloud.aws.credentials.access-key}")
+	private String accessKey;
+
+	@Value("${cloud.aws.credentials.secret-key}")
+	private String secretKey;
+
+	@Value("${cloud.aws.region.static}")
+	private String region;
+	
+	@PostConstruct
+	public void checkProperties() {
+	    System.out.println("Access Key: " + accessKey);
+	    System.out.println("Secret Key: " + secretKey);
+	    System.out.println("Region: " + region);
+	}
+
 	@Bean
 	S3Client s3Client() {
-		return S3Client.builder().region(Region.US_EAST_1) // Replace with your bucket's region
-				.credentialsProvider(DefaultCredentialsProvider.create()).build();
+		System.out.println("Region >>>"+region);
+		AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+		return S3Client.builder().credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+				.region(Region.of(System.getProperty(region))).build();
 	}
 
 }
--- src/main/java/com/bfit/mgmt/config/S3ServiceConfig.java
+++ src/main/java/com/bfit/mgmt/config/S3ServiceConfig.java
@@ -1,21 +1,19 @@
 package com.bfit.mgmt.config;
 
-import java.nio.file.Paths;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
 public class S3ServiceConfig {
-	
+
 	@Autowired
 	private S3Client s3Client;
-
 
 	@Value("${aws.s3.bucket-name}")
 	private String bucketName;
@@ -24,12 +22,16 @@ public class S3ServiceConfig {
 	private String s3BaseUrl;
 
 	public String uploadFile(MultipartFile file, String memberId) {
-		String fileName = memberId + "_" + file.getOriginalFilename();
-		s3Client.putObject(
-				PutObjectRequest.builder().bucket(bucketName).key(fileName).contentType(file.getContentType()).build(),
-				Paths.get(file.getOriginalFilename()));
-		return s3BaseUrl + "/" + fileName;
-
+		try {
+			String fileName = memberId + "_" + file.getOriginalFilename();
+			s3Client.putObject(PutObjectRequest.builder().bucket(bucketName).key(fileName)
+					.contentType(file.getContentType()).build(),
+					RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+			return s3BaseUrl + "/" + fileName;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("File upload failed", e);
+		}
 	}
 
 }
